617ffee3fd231284e8dbababbccfeae8d360ded4
apurva1803 <apurva9raut.com>
2026-03-28 12:40:50 +0530

    done

--- ORSProject-04/src/main/java/in/co/rays/proj4/controller/LoginCtl.java
+++ ORSProject-04/src/main/java/in/co/rays/proj4/controller/LoginCtl.java
@@ -1,5 +1,126 @@
 package in.co.rays.proj4.controller;
 
-public class LoginCtl {
+import java.io.IOException;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import in.co.rays.proj4.bean.BaseBean;
+import in.co.rays.proj4.bean.RoleBean;
+import in.co.rays.proj4.bean.UserBean;
+import in.co.rays.proj4.exception.ApplicationException;
+import in.co.rays.proj4.model.RoleModel;
+import in.co.rays.proj4.model.UserModel;
+import in.co.rays.proj4.util.DataUtility;
+import in.co.rays.proj4.util.DataValidator;
+import in.co.rays.proj4.util.PropertyReader;
+import in.co.rays.proj4.util.ServletUtility;
+
+@WebServlet("/LoginCtl")
+public class LoginCtl extends BaseCtl{
+
+	public static final String OP_SIGN_IN = "Sign In";
+	public static final String OP_SIGN_UP = "Sign Up";
+	public static final String OP_LOG_OUT = "Logout";
+	
+	@Override
+	protected boolean validate(HttpServletRequest request) {
+		
+		boolean pass = true;
+		
+		String op= request.getParameter("operation");
+		
+		if(OP_SIGN_IN.equals(op) || OP_LOG_OUT.equals(op)) {
+			return pass;
+		}
+		
+		if(DataValidator.isNull(request.getParameter("login"))) {
+			request.setAttribute("login", PropertyReader.getValue("error.require", "Login Id"));
+			pass = false;
+		} else if(!DataValidator.isEmail(request.getParameter("login"))) {
+			request.setAttribute("login", PropertyReader.getValue("error.require", "Login"));
+			pass = false;
+		} if(DataValidator.isNull(request.getParameter("password"))) {
+			request.setAttribute("password", PropertyReader.getValue("error.require", "Password"));
+			pass = false;
+		}
+		return pass;
+	}
+	
+	@Override
+	protected BaseBean populateBean(HttpServletRequest request) {
+		
+		UserBean bean = new UserBean();
+		bean.setId(DataUtility.getLong(request.getParameter("id")));
+		bean.setLogin(DataUtility.getString(request.getParameter("login")));
+		bean.setPassword(DataUtility.getString(request.getParameter("password")));
+		
+		return bean;
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		String op = DataUtility.getString(req.getParameter("operation"));
+		
+		if(OP_LOG_OUT.equals(op)) {
+			HttpSession session = req.getSession();
+			session.invalidate();
+			ServletUtility.setSuccessMessage("Logout Successful!", req);
+		}
+		ServletUtility.forward(getView(), req, resp);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		String op= DataUtility.getString(req.getParameter("operation"));
+		
+		UserModel model = new UserModel();
+		RoleModel role = new RoleModel();
+		HttpSession session = req.getSession();
+
+		if(OP_SIGN_IN.equalsIgnoreCase(op)) {
+			
+			UserBean bean = (UserBean) populateBean(req);
+			
+			try {
+				bean = model.authenticate(bean.getLogin(), bean.getPassword());
+				
+				if(bean != null) {
+					session.setAttribute("user", bean);
+					RoleBean rolebean = role.findByPk(bean.getRoleId());
+					
+					if(rolebean != null) {
+						session.setAttribute("role", rolebean.getName());
+					}
+					ServletUtility.redirect(ORSView.WELCOME_CTL, req, resp);
+					return;
+				}else {
+					ServletUtility.setBean(bean, req);
+					ServletUtility.setErrorMessage("Invalid LoginId And Password", req);
+				}
+			} catch (ApplicationException e) {
+				e.printStackTrace();
+				return;
+			}
+		} else if (OP_SIGN_UP.equalsIgnoreCase(op)) {
+			
+			ServletUtility.redirect(ORSView.USER_REGISTRATION_CTL, req, resp);
+			return;
+		}
+		
+		ServletUtility.forward(getView(), req, resp);
+	}
+
+	
+	@Override
+	protected String getView() {
+		return ORSView.LOGIN_VIEW;
+	}
+
+	
 }
